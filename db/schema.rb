346ce33bf2e8eaf3f42639c577bbf685624d35a8
Damir-Che <chechuhov.damir@mail.ru>
2020-12-05 16:12:14 +0300

    Comment(model/controller) for Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_120316) do
+ActiveRecord::Schema.define(version: 2020_12_05_124230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
