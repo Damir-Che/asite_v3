d3e42ea03ead8da85b18255d1a002bbddb094dc9
Damir-Che <chechuhov.damir@mail.ru>
2020-12-06 15:35:32 +0300

    added a form to fill out a user in ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,8 +1,16 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name
+  permit_params :email, :password, :password_confirmation, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name
 
-
+  form do |f|
+    f.inputs "User" do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 
   
 end
